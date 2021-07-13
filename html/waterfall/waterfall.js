020f44e2bff5286f7a378dee5b9328164084bb42
waitingjxr <2364613702.@qq.com>
2021-07-13 10:26:46 +0800

    瀑布流的完全实现

--- html/waterfall/waterfall.js
+++ html/waterfall/waterfall.js
@@ -8,7 +8,27 @@ function imgLocation(parent, content) {
   var cparent = document.getElementById(parent)
   // console.log(cparent)
   var ccontent = getChildElement(cparent, content)
-  console.log(ccontent)
+  // console.log(ccontent)
+  var imgWidth = ccontent[0].offsetWidth
+  var num = Math.floor(document.documentElement.clientWidth / imgWidth)
+  cparent.style.cssText = "width:" + imgWidth * num + "px"
+
+  var BoxHeightArr = []
+  for (var i = 0; i < ccontent.length; i++) {
+    if (i < num){
+      BoxHeightArr[i] = ccontent[i].offsetHeight
+      // console.log(BoxHeightArr[i])
+    } else {
+      // 将Math.min方法借给数组用
+      var minHeight = Math.min.apply(null, BoxHeightArr)
+      var minIndex = getMinHeightLocation(BoxHeightArr, minHeight)
+      // console.log(minHeight)
+      ccontent[i].style.position = 'absolute'
+      ccontent[i].style.top = minHeight + 'px'
+      ccontent[i].style.left = ccontent[minIndex].offsetLeft + 'px'
+      BoxHeightArr[minIndex] = ccontent[i].offsetHeight + BoxHeightArr[minIndex]
+    }
+  }
 }
 
 // 获取父容器中的所有子容器
@@ -22,4 +42,13 @@ function getChildElement(cparent, content){
     }
   }
   return contentArr
+}
+
+// 获取高度最小的图片所在数组的下标
+function getMinHeightLocation(BoxHeightArr, minHeight){
+  for (var i in BoxHeightArr) {
+    if (BoxHeightArr[i] === minHeight) {
+      return i;
+    }
+  }
 }
